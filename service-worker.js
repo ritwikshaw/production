ba8fd43ee60923f949c7c1222abf1bd38831d2cd
Ritwik <ritwikshaw@pluto-2.local>
2021-02-17 10:30:52 +0530

    six

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9058e96eb171535c97cf424c19addc9a.js"
+  "/precache-manifest.3b69a079ea04d058b87f8d4b8b6bc50a.js"
 );
 
 self.addEventListener('message', (event) => {
